36102e4a56c1069e7d8c7b8bad52d831a8d049f9
lucas-prtt <lpresotto@frba.utn.edu.ar>
2024-09-22 18:20:48 -0300

    Pytube dejo de andar así que paso a usar pytubefix, aunque cada tanto tira errores

--- Python/DescargaPlaylist/codigo.py
+++ Python/DescargaPlaylist/codigo.py
@@ -1,4 +1,4 @@
-from pytube import *
+from pytubefix import *
 import os
 
 urlPlaylist = input("Ingrese la URL de la playlist que desea descargar: ")
@@ -7,13 +7,23 @@ pl = Playlist(urlPlaylist)
 print("\nSe ingreso esta playlist: ")
 print(pl.title)
 print("\n")
-
+errores = []
 i = 0    
 for v in pl.videos:
     i=i+1
-    descarga = v.streams.filter(only_audio=True).first()  
-    print(str(i) + ". Descargando: " + v.title + "\n -Tamaño: " + str(descarga.filesize_mb) + "Mb\n")
-    descarga.download(filename_prefix=str(i) + ".",output_path="Audios descargados")
+    try:
+        descarga = v.streams.filter(only_audio=True).first()           
+        print(str(i) + ". Descargando: " + v.title + "\n -Tamaño: " + str(descarga.filesize_mb) + "Mb\n")
+        descarga.download(filename_prefix=str(i) + ".",output_path="Audios descargados")
+    except:
+        errores.append((v,i))
+        print("ERROR: " + v.title + " no disponible\nPasando al siguiente..\n")
+
+print("\n\nVideos no descargados:\n\n")
+
+
+for x in errores:
+        print("No se descargo " + str(x[1]) + ". " + x[0].title + "\n\n")
 
 path="./Audios descargados"
 for archivo in os.listdir(path):
