929846eb741a0c7d7ce8daf0d1ce92d9661d5209
lucas-prtt <lpresotto@frba.utn.edu.ar>
2024-12-15 19:58:15 -0300

    pruebas

--- Python/JsonParser/test.py
+++ Python/JsonParser/test.py
@@ -1,8 +1,8 @@
 from parser import *
 
-#fileName = input("File name: ")
-#fileName = "example.json"
-#file = open(fileName, "r")
-#fileContent = file.read()
-#parseJson(fileContent)
-parseJson(input())
+fileName = input("File name: ")
+file = open(fileName, "r")
+fileContent = file.read()
+elemento = parseJson(fileContent)
+
+print(vars(elemento))
