dcc91580e5f82e8c6098370fc4382fbdbc9ed19b
lucas-prtt <lpresotto@frba.utn.edu.ar>
2024-12-15 17:03:59 -0300

    Modifico nuevoObjeto para usar listas

--- Python/JsonParser/parser.py
+++ Python/JsonParser/parser.py
@@ -1,15 +1,26 @@
-
 class nuevoObjeto:
-    def __init__(self, *atributos):   # Atributos es uno o mas duplas (identificador, valor)
+    def __init__(self, atributos):   # Atributos es uno o mas duplas (identificador, valor)
         for attr in atributos:
             self.nuevoAtributo(attr[0], attr[1])
         
         def nuevoAtributo(self, id, val):
             setattr(self, id, val)
 
+
 def parseJson(jsonString):
-    # x = nuevoObjeto(("numeroDeLaSuerte", 7), ("edad", 60), ("nombre", "Manuel Belgrano"))
-    pass
+    tempJsonString = jsonString
+    return matchObjeto(tempJsonString)
+    # x = nuevoObjeto([("numeroDeLaSuerte", 7), ("edad", 60), ("nombre", "Manuel Belgrano")])
+
+
+
+def matchObjeto(string):
+    atributos = []
+    # match abreCorchete
+    # match atributos
+    # match cierraCorchete
+    return nuevoObjeto(atributos)
+
 
 
 
