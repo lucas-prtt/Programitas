c3b19808540aca23fa0db259d57d38eb52aa28c9
lucas-prtt <lpresotto@frba.utn.edu.ar>
2024-12-15 18:41:44 -0300

    Reconoce strings como atributos

--- Python/JsonParser/parser.py
+++ Python/JsonParser/parser.py
@@ -6,8 +6,8 @@ class nuevoObjeto:
         for attr in atributos:
             self.nuevoAtributo(attr[0], attr[1])
         
-        def nuevoAtributo(self, id, val):
-            setattr(self, id, val)
+    def nuevoAtributo(self, id, val):
+        setattr(self, id, val)
 
 
 def parseJson(jsonString):
@@ -18,37 +18,104 @@ def parseJson(jsonString):
 
 
 
+#################
+#### match's ####
+#################
+
+
 def matchObjeto():
     atributos = []
     matchCaracter("{")
 
-    # match atributo
-    # while (proximoCaracter(","))
-    # {
-    # match atributo
-    # }
-
+    if proximoCaracter() != "}": 
+        atributos.append(matchAtributo())
+        while (proximoCaracter() == ','):
+            quitarPrimerCaracter()
+            atributos.append(matchAtributo())
 
     matchCaracter("}")
     return nuevoObjeto(atributos)
 
 
 
+def matchAtributo():
+    id = matchTexto()
+    quitarEspacios()
+    matchCaracter(":")
+    quitarEspacios()
+    valor = matchValor()
+    return (id, valor)
 
+    
+
+def matchTexto():
+    texto = ""
+    matchCaracter('"')
+    while proximoCaracter() != '"':
+        texto = texto + (proximoCaracter())
+        quitarPrimerCaracter()
+    matchCaracter('"')
+    return texto
+    
+def matchValor():
+    if proximoCaracter() == '"':
+        return matchTexto()
+    elif proximoCaracter() in "0123456789.":
+        return matchNumero()
+    elif proximoCaracter() == "[":
+        return matchArray()
+    elif proximoCaracter() == "{":
+        return matchObjeto()
+    elif proximaPalabra("true"):
+        quitarCaracteres(4)
+        return True
+    elif proximaPalabra("false"):
+        quitarCaracteres(5)
+        return False
+    elif proximaPalabra("null"):
+        quitarCaracteres(4)
+        return None
+    else:
+        raise Exception("Error en funcion matchValor: tipo de valor no identificado")
+    
+
+
+
+#############################################################
+########### Funciones que editan globalJsonString ###########
+#############################################################
 
 def matchCaracter(caracter):
     global globalJsonString
     if (globalJsonString[0] != caracter) :
         raise Exception("Se esperaba " + caracter + " pero se recibio " + globalJsonString[0])
     else :
-        globalJsonString = globalJsonString[1:]
+        quitarPrimerCaracter()
         return
 
-def proximoCaracter(caracter):
+def proximoCaracter():
     global globalJsonString
-    if (globalJsonString[0] != caracter) :
-        return False
-    else:
-        return True
+    return globalJsonString[0]
+def quitarPrimerCaracter():
+    global globalJsonString
+    globalJsonString = globalJsonString[1:]
+
+def proximaPalabra(palabra):
+    global globalJsonString
+    temp = globalJsonString
+    for c in palabra:
+        if temp[0] != c:
+            return False
+        temp = temp[1:]
+    return True
+
+def quitarCaracteres(cuantos):
+    for i in range(cuantos):
+        quitarPrimerCaracter()
+
+def quitarEspacios():
+    global globalJsonString
+    while globalJsonString[0] == " ":
+        quitarPrimerCaracter()
 
 
--- Python/JsonParser/test.py
+++ Python/JsonParser/test.py
@@ -1,8 +1,8 @@
 from parser import *
 
 #fileName = input("File name: ")
-fileName = "example.json"
-file = open(fileName, "r")
-fileContent = file.read()
-
-parseJson(fileContent)
+#fileName = "example.json"
+#file = open(fileName, "r")
+#fileContent = file.read()
+#parseJson(fileContent)
+parseJson(input())
