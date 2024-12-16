3ae740043017878a9d6b3c9454dffb5d533db491
lucas-prtt <lpresotto@frba.utn.edu.ar>
2024-12-15 22:37:36 -0300

    Actualizacion menu

--- Python/ProductoMatricial/productoMatricial.py
+++ Python/ProductoMatricial/productoMatricial.py
@@ -1,8 +1,6 @@
-from imprimirMatriz import imprimirMatriz
+from imprimirMatriz import imprimirMatriz, columnasMatriz, filasMatriz
 from ingresarMatriz import ingresarMatriz
 opcion = "-1"
-matriz1 = [[1, 2],[3, 4],[5, 6]]
-matriz2 = [[1, 2, 3],[4, 5, 6]]
 
 while opcion != "0":
     print("Programa para realizar operaciones con matrices:")
@@ -26,7 +24,7 @@ while opcion != "0":
         case "2":
             matriz2 = ingresarMatriz()
         case "3":
-            print("Matriz 1: ")
+            print("\nMatriz 1: ")
             imprimirMatriz(matriz1)
             print("Matriz 2: ")
             imprimirMatriz(matriz2)
@@ -35,11 +33,13 @@ while opcion != "0":
             matriz1 = matriz2
             matriz2 = temp
         case "5":
-            resultado = productoMatricial(matriz1, matriz2)
-            print("Resultado de la multiplicacion: ")
-            imprimirMatriz(resultado)
-
-
+            if columnasMatriz(matriz1) == filasMatriz(matriz2):
+                resultado = productoMatricial(matriz1, matriz2)
+                print("Resultado de la multiplicacion: ")
+                imprimirMatriz(resultado)
+            else: 
+                print("Error: las columnas de la matriz 2 deben ser iguales a las filas de la matriz 1. ")
+            input()
 
 #if m1 != n2 :
 #    input("Error: las columnas de la matriz 2 deben ser iguales a las filas de la matriz 1. ")
