e1be7d4f947671de9df758632af899bb38bb1d43
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-01-08 14:54:59 -0300

    Generate table from matrix

--- Javascript/Tateti/table.js
+++ Javascript/Tateti/table.js
@@ -1,17 +1,19 @@
-function generateEmptyTable(rows, columns){
+function generateTable(matrix){
+    columns = columnsAmount(matrix)
+    rows = rowsAmount(matrix)
     const table = document.createElement("table");
     const tableBody = document.createElement("tbody");
     for (i = 0; i<rows; i++){
         const row = document.createElement("tr");
         for(j = 0; j<columns; j++){
-            row.appendChild(createCell("\u{00a0}".repeat(5)));
+            row.appendChild(createCell(matrix[i][j]));
         }
         tableBody.appendChild(row);
     }
     table.appendChild(tableBody);
-    document.body.appendChild(table);
     table.setAttribute("border", "3");
     table.style.padding = "2px";
+    return table
 }
 
 function createCell(text){
@@ -20,4 +22,27 @@ function createCell(text){
     cell.appendChild(cellText);
     cell.style.padding = "6px"
     return cell;    
+}
+
+function generateEmptyTable(rows, columns){
+    var matrix = []
+    for (i=0; i<columns; i++){
+        matrix.push([])
+        for (j=0; j<columns; j++){
+            matrix[i].push("\u00a0".repeat(5))
+        }
+    }
+    var table = generateTable(matrix)
+    return table
+}
+
+function rowsAmount(matrix){
+    return matrix.length
+}
+function columnsAmount(matrix){
+    var col = 0
+    for (i = 0; i<rowsAmount(matrix); i++){
+        col = Math.max(col, matrix[i].length)
+    }
+    return col
 }
