edc08b74583c4850536e4c212566a44df7e8c725
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-03-15 20:01:15 -0300

    Line fix

--- Python/Grid-triangulation/utils/graphics.py
+++ Python/Grid-triangulation/utils/graphics.py
@@ -6,10 +6,12 @@ im = Image.new("RGB", (1024, 1024))
 def reset(size):
     im = Image.new("RGB", size)
 def paint(position, color):
-    im.putpixel(position, color)
+    im.putpixel((position[0], im.height-position[1]), color)
 def line(slope, offset, color):
     for x in range(0, im.width):
-        paint((x, min(Tr.evaluate(x, slope, offset), im.height-1)), color)
+        pos = (x, Tr.evaluate(x, slope, offset))
+        if pos[1] <= im.height:
+            paint(pos, color)
 
 line(3, 400, (255, 0, 0))
 
